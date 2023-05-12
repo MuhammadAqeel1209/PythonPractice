96a752b07b0fe13b5829d7b37c2171b96ba9df3f
Muhammad Aqeel <129723771+MuhammadAqeel1209@users.noreply.github.com>
2023-05-12 22:37:51 +0500

    Add files via upload

---
+++ is_aur_==operator.py
@@ -0,0 +1,18 @@
+#is--> operator compare the exact location of memory
+# ==--> compare the values
+#BOTH ARE COMPARISON OPERATOR
+
+# a = 4
+# b = "4"
+# a = [1, 2, 43]
+# b = [1, 2, 43]
+# a = 3
+# b = 3
+# a = (1,2)
+# b = (1,2)
+a = None
+b = None
+print(a is b)
+print(a == b) 
+print(a is None)
+print(b is None)
