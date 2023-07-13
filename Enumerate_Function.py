78d92a3b4a95dc9359ea3ad291763e6860e28f87
Muhammad Aqeel <129723771+MuhammadAqeel1209@users.noreply.github.com>
2023-07-13 17:27:43 +0500

    Add files via upload

---
+++ Enumerate_Function.py
@@ -0,0 +1,16 @@
+# marks = [12, 34, 56, 98, 12, 45, 4]
+
+# index = 0
+# for mark in marks:
+#     print(mark)
+#     if(index == 4):
+#         print("Execellent")
+#     index += 1    
+
+marks = [12, 34, 56, 98, 12, 45, 4]
+
+for index, mark in enumerate( marks):
+    # print(mark)
+    # if(index == 3):
+    # print(index,"Execellent")  
+    print(index,mark)
